5aa7dc94cc748e300edbd9bb1559bb1593681dd7
ritikjoshi22 <joshster.ritik@gmail.com>
2024-07-07 15:50:00 +0545

    binary search tree

---
+++ tree/bst.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* left;
+    node* right;
+
+    node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+node* insertBST(node* root,int val)
+{
+    if(root == NULL){
+        return new node(val);
+    }
+    if(val<root->data){
+        root->left=insertBST(root->left,val);
+    }
+    else{
+            //val>root->data
+        root->right=insertBST(root->right,val);
+    }
+    return root;
+}
+
+void inorder(node* root)
+{
+    if(root == NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+int main()
+{
+    node* root = NULL;
+    root = insertBST(root,5);
+    insertBST(root,1);
+    insertBST(root,3);
+    insertBST(root,4);
+    insertBST(root,2);
+    insertBST(root,7);
+    //To print the bst
+    inorder(root);
+    cout<<endl;
+    return 0;
+}
